6e0c85e4daab63d4b62e49550012ec395772b712
JA54312 <ija54312@gmail.com>
2023-03-24 11:56:33 -0600

    map

---
+++ server/02-map.js
@@ -0,0 +1,12 @@
+const letters = ['a','b','c'];
+
+
+const newArray = letters.map(item => item + '++')
+//const newArray = [];
+
+// for(let i= 0;i<letters.length;i++){
+//     const element = letters[i];
+//     newArray.push(element+'++');
+// }
+console.log('original',letters)
+console.log('nuevo',newArray)
